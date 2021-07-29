be47d05b9701c59d1816736b70a3b22131372e99
simlecode <hj1557986@gmail.com>
2021-07-29 13:46:26 +0800

    add flag

--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/hunjixin/grafana-simplejson-mongo/api"
 )
 
 func main() {
+	var port int
+	var mongoHost string
+	flag.StringVar(&mongoHost, "mongodb", "mongodb://localhost", "mongodb host")
+	flag.IntVar(&port, "port", 8082, "server port")
+	flag.Parse()
+	log.Printf("port: %d, mongodb host: %s\n", port, mongoHost)
 	conf := api.Config{
-		Port:      8080,
-		MongoHost: "mongodb://127.0.0.1:27017",
+		Port:      port,
+		MongoHost: mongoHost,
 	}
 	errs := make(chan error, 2)
 	api.StartHTTPServer(conf, errs)
-	log.Println("start")
+	log.Println("server start")
 	for {
 		err := <-errs
 		log.Println(err)
